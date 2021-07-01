c47619c216c56502cb10bd879eeadb01e878018e
LevitMary <86655340+LevitMary@users.noreply.github.com>
2021-07-01 12:19:37 +0300

    Update 01_task_func.py

--- Module4/practice/funcs_part1/01_task_func.py
+++ Module4/practice/funcs_part1/01_task_func.py
@@ -11,3 +11,17 @@ print(max2(-10, -12))
 print(max2(2.5, 2.6))
 print(max2(-2.5, 0))
 print(max2(0, -2.5))
+
+def max2(n1, n2):
+    if n1>n2 :
+        res=n1
+    else:
+        res=n2
+    return res
+
+# Тестируем функцию
+print(max2(5, 6))
+print(max2(-10, -12))
+print(max2(2.5, 2.6))
+print(max2(-2.5, 0))
+print(max2(0, -2.5))
